0634b55db081f074f0029201c9d855d307176264
olaf-baranski <157897672+olaf-baranski@users.noreply.github.com>
2026-03-20 09:20:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
-from fastapi.testclient import TestClient
-from main import app
+from fastapi import FastAPI
 
-client = TestClient(app)
+app = FastAPI()
 
-def test_hello():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+@app.get("/")
+def hello():
+    return {"message": "Hello World"}
 
-def test_add_positive_numbers():
-    response = client.get("/add/2/3")
-    assert response.status_code == 200
-    assert response.json() == {"result": 5}
-
-def test_add_negative_numbers():
-    response = client.get("/add/-2/1")
-    assert response.status_code == 200
-    assert response.json() == {"result": -1}
+@app.get("/add/{a}/{b}")
+def add(a: int, b: int):
+    return {"result": a + b}
